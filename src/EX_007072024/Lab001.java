356b68c071cedb9184ab72ebd5b1bdb9daa62974
kiran <kiran.gicts@gmail.com>
2024-07-11 17:29:27 +0530

    all files added

--- src/EX_007072024/Lab001.java
+++ src/EX_007072024/Lab001.java
@@ -1,2 +1,8 @@
-package EX_007072024;public class Lab001 {
+package EX_007072024;
+
+public class Lab001 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, this is My first program");
+    }
 }
