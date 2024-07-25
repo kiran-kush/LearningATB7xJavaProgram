0b7976d0568cab68d85ec7d95d5c0bd3d892dddf
kiran <kiran.gicts@gmail.com>
2024-07-25 22:45:03 +0530

    Home work added

---
+++ src/Home_Work_20_July/Leap_Year.java
@@ -0,0 +1,17 @@
+package Home_Work_20_July;
+
+import java.util.Scanner;
+
+public class Leap_Year {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter any year");
+        int year= sc.nextInt();
+        if(year % 4 == 0 || year % 400 == 0){
+            System.out.println("this is a leap year");
+
+        }else{
+            System.out.println("Not a leap year");
+        }
+    }
+}
